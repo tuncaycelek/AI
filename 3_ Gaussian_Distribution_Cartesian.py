f84f684b479e0a053c1284d7c0fa3f3cdd87d248
tuncaycelek <tuncayc@gmail.com>
2025-10-13 11:23:39 +0300

    Add files via upload

--- 3_ Gaussian_Distribution_Cartesian.py
+++ 3_ Gaussian_Distribution_Cartesian.py
@@ -1,28 +1,33 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 def gauss(x, mu = 0, std = 1):
     return 1 / (std * np.sqrt(2 * np.pi)) * np.e ** (-0.5 * ((x - mu) / std) ** 2)
-  
-def draw_gauss(mu = 0, std = 1):
+      
+def fill_gauss(mu = 0, std = 1, fstart= 0, fstop = 0):
     x = np.linspace(-5 * std + mu, 5 * std + mu, 1000)
     y = gauss(x, mu, std)
     
     mu_y = gauss(mu, mu, std)
     
-    plt.figure(figsize=(20, 8))
+    plt.figure(figsize=(10, 4))
     plt.title('Gauss Function', pad=10, fontweight='bold')
     axis = plt.gca()
     
     axis.set_ylim([-mu_y * 1.1, mu_y * 1.1])
     axis.set_xlim([-5 * std + mu, 5 * std + mu])
     axis.set_xticks(np.arange(-4 * std + mu, 5 * std + mu, std))
-    # axis.set_yticks(np.round(np.arange(-mu_y, mu_y, mu_y / 10), 2))
+   # axis.set_yticks(np.round(np.arange(-mu_y, mu_y, mu_y / 10), 2))
     axis.spines['left'].set_position('center')
     axis.spines['top'].set_color(None)
     axis.spines['bottom'].set_position('center')
     axis.spines['right'].set_color(None)
     axis.plot(x, y)
+    
+    x = np.linspace(fstart, fstop, 1000)
+    y = gauss(x, mu, std)
+    axis.fill_between(x, y)
     plt.show()
 
-draw_gauss(100, 15)
+fill_gauss(100, 15, 85, 115)
