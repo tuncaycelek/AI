e54e8de568921d5c0235710a828678bed5a29e2d
tuncaycelek <tuncayc@gmail.com>
2025-10-16 14:49:52 +0300

    Add files via upload

---
+++ 6_Continuous_Uniform_Distribution_Basic_Sample.py
@@ -0,0 +1,26 @@
+import numpy as np
+from scipy.stats import uniform
+import matplotlib.pyplot as plt
+
+A = 10
+B = 20
+
+x = np.linspace(A - 5, B + 5, 1000)
+y = uniform.pdf(x, A, B - A)
+
+plt.title('Continuous Uniform Distribution', fontweight='bold')
+plt.plot(x, y)
+
+x = np.linspace(10, 12.5, 1000)
+y = uniform.pdf(x, A, B - A)
+plt.fill_between(x, y)
+plt.show()
+
+result = uniform.cdf(12.5, A, B - A)
+print(result)                               # 0.25
+
+result = uniform.ppf(0.5, A, B - A)
+print(result)                               # 15
+
+x = uniform.rvs(0, 1, 10)       # yukarıdakiyle tamamen aynı biçimde rassal sayı üretir
+print(x)
