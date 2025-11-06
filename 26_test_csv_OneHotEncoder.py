7d334c5f574f6e9d593814a73b96ec5a631d9f23
tuncaycelek <tuncayc@gmail.com>
2025-11-07 01:46:56 +0300

    Add files via upload

---
+++ 26_test_csv_OneHotEncoder.py
@@ -0,0 +1,18 @@
+# https://github.com/tuncaycelek/AI/blob/main/DataSets/test.csv
+ 
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+
+df = pd.read_csv('DataSets\\test.csv')
+
+ohe = OneHotEncoder(sparse_output=False, dtype='uint8')
+transformed_data = ohe.fit_transform(df[['RenkTercihi', 'Meslek']])
+df.drop(['RenkTercihi', 'Meslek'], axis=1, inplace=True)
+
+categories1 = ['RenkTercihi_' + category for category in ohe.categories_[0]]
+categories2 = ['Meslek_' + category for category in ohe.categories_[1]]
+
+df[categories1 + categories2] = transformed_data
+
+print(df)
+
